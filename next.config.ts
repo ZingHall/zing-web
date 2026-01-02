c4f0d048a50e100f806618c106a0f1407ec86d96
Jarek Lin <s410673074@gmail.com>
2026-01-02 13:46:50 +0800

    feat: add walrus package to external module

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // This is required for WASM modules to work in Next.js
-    webVitalsAttribution: ["CLS", "LCP"],
-  },
-  webpack: (config, { isServer }) => {
-    // Enable asyncWebAssembly
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
-  },
+  output: "standalone",
+  serverExternalPackages: ["@mysten/walrus", "@mysten/walrus-wasm"],
 };
 export default nextConfig;
