27764c5c09e1e124d3d391376b934de4def8899f
Jarek Lin <s410673074@gmail.com>
2026-01-02 13:28:23 +0800

    feat: update next.config.ts to support wasm

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  serverExternalPackages: ["@mysten/walrus-wasm"],
+  experimental: {
+    // This is required for WASM modules to work in Next.js
+    webVitalsAttribution: ["CLS", "LCP"],
+  },
+  webpack: (config, { isServer }) => {
+    // Enable asyncWebAssembly
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 export default nextConfig;
