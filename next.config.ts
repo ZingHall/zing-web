30c283a69656cc70675d0c066e55c6f1bd4e5ebb
Jarek Lin <s410673074@gmail.com>
2026-01-02 13:20:18 +0800

    feat: add walrus-wasm external module

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["@mysten/walrus-wasm"],
 };
 export default nextConfig;
