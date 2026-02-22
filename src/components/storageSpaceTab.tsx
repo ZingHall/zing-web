66cd73bdcd4b28252b4b381057f8fd93e3afb419
Jarek Lin <s410673074@gmail.com>
2026-02-22 23:21:27 +0800

    feat: reset when refetch

--- src/components/storageSpaceTab.tsx
+++ src/components/storageSpaceTab.tsx
@@ -280,6 +280,7 @@ export default function StorageStatusPage() {
             <button
               className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700"
               onClick={async () => {
+                client.walrus.reset();
                 await refetchWalrusSystem();
                 await refetchStorageTreasury();
               }}
